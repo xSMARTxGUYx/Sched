7c3a2e75a9fab7a60bc6ece94c0080433648c99c
xSMARTxGUYx <arendlife021@gmail.com>
2024-01-02 22:59:18 +0200

    first commit

---
+++ src/main/java/com/performances/repository/EventRepository.java
@@ -0,0 +1,10 @@
+package com.performances.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.performances.sched.entity.Event;
+
+public interface EventRepository extends JpaRepository<Event, String> {
+
+    
+} 
