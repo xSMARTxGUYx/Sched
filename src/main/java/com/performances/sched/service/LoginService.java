e0eabeb3d21f1a1ffa937635369eb45678424e33
xSMARTxGUYx <arendlife021@gmail.com>
2024-02-07 10:54:52 +0200

    Update Commit

--- src/main/java/com/performances/sched/service/LoginService.java
+++ src/main/java/com/performances/sched/service/LoginService.java
@@ -2,7 +2,17 @@ package com.performances.sched.service;
 
 import org.springframework.stereotype.Service;
 
+import com.performances.sched.repository.AdminRepository;
+import com.performances.sched.repository.CustomerRepository;
+
 @Service
 public class LoginService {
-    
+    private final AdminRepository adminRep;
+    private final CustomerRepository customerRep;
+
+    public LoginService(AdminRepository adminRep, CustomerRepository customerRep) {
+        this.adminRep = adminRep;
+        this.customerRep = customerRep;
+    }
+
 }
